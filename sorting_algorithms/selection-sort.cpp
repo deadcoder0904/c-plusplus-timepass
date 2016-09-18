9370df33eb59572272d1efafdc5ceeed8684539a
Akshay Kadam <akshaykadam0904@gmail.com>
2016-09-18 19:26:58 +0530

    Adding script on Sun Sep 18 19:26:58 IST 2016 :blush:

--- sorting_algorithms/selection-sort.cpp
+++ sorting_algorithms/selection-sort.cpp
@@ -27,6 +27,7 @@ typedef long double ld;
 const float PI=3.1415926535897932384626433832795;
 const ull MOD=1e9+7;
 
+void swap(int *a,int *b){int temp=*a;*a=*b;*b=temp;}
 bool even(ull x) { return x%2==0?true:false; }
 bool odd(ull x) {return x%2==1?true:false; }
 ull fact(ull a){ ull f=1; REP2(i,1,a+1) f*=i; return f;}
@@ -38,10 +39,10 @@ ll fast_expo(ll a,ll b)
     if(b==2) return a*a;
     return b&1 ? a*fast_expo(fast_expo(a,(b-1)/2),2) : fast_expo(fast_expo(a,b/2),2);
 }
-void input_array(int *a,int n){ REP(i,n) cin>>a[i];}
-void print_array(int *a,int n){ REP(i,n) cout<<a[i]; cout<<endl;}
+void input_array(int a[],int n){ REP(i,n) cin>>a[i];}
+void print_array(int a[],int n){ REP(i,n) cout<<a[i]<<" "; cout<<endl;}
 
-void selection_sort(int *a,int n){
+void selection_sort(int a[],int n){
 	REP(i,n-1){
 		int minI=i;
 		REP2(j,i+1,n)
@@ -59,9 +60,9 @@ int main()
 		int n;
 		cin>>n;
 		int a[n];
-		input_array(&a,n);
+		input_array(a,n);
 		selection_sort(a,n);
-		print_array(&a,n);
+		print_array(a,n);
 	}
 	return 0;
 }
