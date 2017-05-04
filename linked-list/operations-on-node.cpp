2d8bd742fee1be622210e8d64d087ddee5c84734
Akshay Kadam <akshaykadam0904@gmail.com>
2017-05-04 10:41:38 +0530

    Alternate Splitting of Linked List :lipstick:

--- linked-list/operations-on-node.cpp
+++ linked-list/operations-on-node.cpp
@@ -562,6 +562,18 @@ void deleteAlternateNodesRecursive(node *head) {
 	deleteAlternateNodesRecursive(head->next);
 }
 
+void alternateSplitting(node *head, node **x, node **y) {
+	if(head == NULL) return;
+	int count = 1;
+	while(head != NULL) {
+		if(count%2 == 1)
+			insertAtEnd(x, head->data);
+		else insertAtEnd(y, head->data);
+		count++;
+		head = head->next;
+	}
+}
+
 int main()
 {
 	node *head = NULL;
@@ -725,5 +737,11 @@ int main()
 	deleteAlternateNodesRecursive(head55);
 	printList(head55);
 
+	printList(head);
+	cout<<"Alternate Splitting of Linked List : "<<endl;
+	node *x = NULL, *y = NULL;
+	alternateSplitting(head, &x, &y);
+	printList(x);
+	printList(y);
 	return 0;
 }
